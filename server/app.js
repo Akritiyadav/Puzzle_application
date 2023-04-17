6e29e5d9092ca25612953f7bbbcd41789d814136
Akritiyadav <akritiyadav895@gmail.com>
2023-04-17 06:37:56 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const  dotenv =require('dotenv');
+// const  dotenv =require('dotenv');
 const mongoose = require("mongoose");
 app.use(express.json());
 const cors = require("cors");
@@ -11,16 +11,19 @@ app.use(express.urlencoded({ extended: false }));
 
 const jwt = require("jsonwebtoken");
 var nodemailer = require("nodemailer");
-const JWT_SECRET =
-  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
 
-  const mongoUrl =
-  "mongodb+srv://akritiyadav895:Akriti_1010@cluster0.mcsiymy.mongodb.net/?retryWrites=true&w=majority";
 
- dotenv.config();
-mongoose
-  .connect(mongoUrl, {
+ require("dotenv").config();
+
+ const JWT_SECRET =
+ "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+
+ const mongoUrl =
+ "mongodb+srv://akritiyadav895:Akriti_1010@cluster0.mcsiymy.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
+    useUnifiedTopology: "true"
   })
   .then(() => {
     console.log("Connected to database");
@@ -98,6 +101,8 @@ app.post("/userData", async (req, res) => {
       });
   } catch (error) { }
 });
+
+
 
  const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
